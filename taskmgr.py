8beeaff93bbd7e932136bf5cbda629f0bedaca07
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2020-05-27 14:13:38 +0100

    Test: created tasks.html and base.html. Using flask_pymongo to connect to the MongoDB.

--- taskmgr.py
+++ taskmgr.py
@@ -1,12 +1,27 @@
 import os
-from  flask  import  Flask
+from  flask  import  Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid  import ObjectId
+
+
+from dotenv import load_dotenv
+from pathlib import Path
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI_TM")
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/get_tasks")
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=os.environ.get("PORT"), debug=True)
-    
+
